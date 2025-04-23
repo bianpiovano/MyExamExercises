335ae9f39965afdfd0b7ca54cc1747cf720d2d17
bianpiovano <bianpiovano@gmail.com>
2025-04-23 21:49:23 +0200

    making this code cuter

--- Exam02/alpha_mirror.c
+++ Exam02/alpha_mirror.c
@@ -1,25 +1,18 @@
 #include <unistd.h>
 
-void mirror(char *str)
+void alpha_mirror(char *str)
 {
 	int i = 0;
 	char newchar;
-	
 
 	while(str[i] != '\0')
 	{
 		if (str[i] >= 'a' && str[i] <= 'z')
-		{
 			newchar = 'a' + ('z' - str[i]);
-		}
 		else if (str[i] >= 'A' && str[i] <= 'Z')
-		{
 			newchar = 'A' + ('Z' - str[i]);
-		}
 		else
-		{
 			newchar = str[i];
-		}
 		write(1, &newchar, 1);
 		i++;
 	}
@@ -29,8 +22,6 @@ void mirror(char *str)
 int main(int argc, char *argv[])
 {
 	if (argc == 2)
-	{
-		mirror(argv[1]);
-	}
+		alpha_mirror(argv[1]);
 	write(1, "\n", 1);
 }
